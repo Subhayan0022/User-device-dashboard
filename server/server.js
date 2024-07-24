3ef7234cba3a30c2db0f317dac90765a3aa0f031
Subhayan <msubhayan529@gmail.com>
2024-07-24 17:32:11 +0530

    backend connection established

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 
 const PORT = 8080;
 
-
+app.use(cors());
 
 app.get("/api/home", (req, res) => {
     res.json({
